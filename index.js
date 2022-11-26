e14162def6998918600a1001d60eeab4c088d2ed
alow-akter <kironalow453@gmail.com>
2022-11-25 21:20:27 -0500

    all category commit

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const cors = require('cors')
 const port = process.env.PORT || 5001
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 
 const app = express()
@@ -13,13 +13,39 @@ app.use(express.json())
 
 
 
-const uri = "mongodb+srv://<username>:<password>@cluster0.sduwbrc.mongodb.net/?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.sduwbrc.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-    const collection = client.db("test").collection("devices");
-    // perform actions on the collection object
-    client.close();
-});
+
+
+
+async function run() {
+    try {
+        const allHeadphonesCollection = client.db('headphonesDatabase').collection('threeBrandsHeadphone')
+
+        const allCategoryCollection = client.db('headphonesDatabase').collection('allCategory')
+
+        app.get('/headphonesOptions', async (req, res) => {
+            const query = {}
+            const options = await allHeadphonesCollection.find(query).toArray()
+            res.send(options)
+        })
+        app.get('/categoryOptions/:id', async (req, res) => {
+            const id = req.params.id;
+            const filter = { productsId: id };
+
+            const result = await allCategoryCollection.find(filter).toArray();
+
+            res.send(result);
+        })
+    }
+    finally {
+
+    }
+
+}
+run().catch(console.log)
+
+
 
 
 app.get('/', async (req, res) => {
